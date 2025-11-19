2796620b997e021c395665a040d7377814bc24b5
Susan <tszhuen.zuo@winsor.edu>
2025-11-19 15:53:02 -0500

    Create BillyMA.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Mechs/Assemblies/BillyMA.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.RobotModel.Mechs.Assemblies;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class BillyMA extends MechAssembly
+{
+    public BillyMA(HardwareMap hardwareMap) {
+
+    }
+
+    @Override
+    public <T extends AutonomousMechBehaviors> T getAutonomousBehaviors() {
+        return null;
+    }
+
+    @Override
+    public void giveInstructions(Gamepad gamepad) {
+
+    }
+
+    @Override
+    public void updateTelemetry(Telemetry telemetry) {
+
+    }
+}
