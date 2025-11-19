48442a86f50c7cd975d939de0e72d74ae7618090
Eleanor Chen <eleanor.chen@winsor.edu>
2025-11-19 15:49:30 -0500

    mech assembly billy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robots/BillyRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robots/BillyRobot.java
@@ -30,5 +30,7 @@ public class BillyRobot extends Robot {
                 DcMotorSimple.Direction.FORWARD,
                 DcMotorSimple.Direction.REVERSE
         ));
+
+        mechAssembly = new BillyMA(hardwareMap);
     }
 }
