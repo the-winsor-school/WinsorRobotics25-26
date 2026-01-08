80dc62e0a3bf9d2134782b9307eadcfa4c32f0dc
Susan <tszhuen.zuo@winsor.edu>
2026-01-08 15:44:33 -0500

    auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonStrategies/idealAutonStrat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonStrategies/idealAutonStrat.java
@@ -12,7 +12,9 @@ public class idealAutonStrat {
      */
     public static IAutonStrategy BasicAutonomous(BillyRobot.AutonomousMecanumRobot robot) {
         return () -> {
-            //insert code
+
         };
     }
+
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robots/BillyRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robots/BillyRobot.java
@@ -1,18 +1,56 @@
 package org.firstinspires.ftc.teamcode.RobotModel.Robots;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Size;
+
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.CameraControl;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.RobotModel.DriveTrain.Mecanum.MecanumDrive;
 import org.firstinspires.ftc.teamcode.RobotModel.Mechs.Assemblies.BillyMA;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.Mat;
+
+import java.util.Collections;
+import java.util.List;
 
 public class BillyRobot extends Robot {
-    @Override
-    public <T extends AutonomousRobot> T getAutonomousRobot() {
+    public class AutonomousMecanumRobot extends AutonomousRobot
+    {
+        public final MecanumDrive.AutonomousMecanumDrive driveTrain;
+        public final BillyMA.AutonomousBillyMA mechAssembly;
 
-        return null;
+        public AutonomousMecanumRobot(MecanumDrive.AutonomousMecanumDrive driveTrain,
+                                      BillyMA.AutonomousBillyMA mechAssembly)
+        {
+            super(driveTrain, mechAssembly);
+            this.driveTrain = driveTrain;
+            this.mechAssembly = mechAssembly;
+        }
     }
 
+    private final AprilTagProcessor aprilTag;
+    private final ColorBlobLocatorProcessor purple;
+    private final ColorBlobLocatorProcessor green;
+
+
+    private final AutonomousMecanumRobot auton;
+    @Override
+    public BillyRobot.AutonomousMecanumRobot getAutonomousRobot() {
+        return auton;
+    }
 
     public BillyRobot(HardwareMap hardwareMap) {
         driveTrain = new MecanumDrive(hardwareMap, new MecanumDrive.OrientationConfiguration(
@@ -23,5 +61,35 @@ public class BillyRobot extends Robot {
         ));
 
         mechAssembly = new BillyMA(hardwareMap);
+
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+        purple = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setRoi(ImageRegion.entireFrame())
+                .setDrawContours(true)
+                .build();
+
+        green = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.GREEN)
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setRoi(ImageRegion.entireFrame())
+                .setDrawContours(true)
+                .build();
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "webcam"))
+                .setCameraResolution(new Size(640, 480))
+                .addProcessor(aprilTag)
+                .addProcessor(purple)
+                .addProcessor(green)
+                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
+                .enableLiveView(true)
+                .build();
+
+
+        auton = new BillyRobot.AutonomousMecanumRobot(
+                driveTrain.getAutonomousDriving(),
+                mechAssembly.getAutonomousBehaviors());
     }
 }
