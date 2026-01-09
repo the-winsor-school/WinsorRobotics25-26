9fb9927d0665cc36228e67072f3fe2b57637d5b5
Eleanor Chen <eleanor.chen@winsor.edu>
2026-01-08 20:58:43 -0500

    right auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonStrategies/rightSideAuton.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.AutonStrategies;
+
+import org.firstinspires.ftc.teamcode.Extensions.ThreadExtensions;
+import org.firstinspires.ftc.teamcode.RobotModel.Robots.BillyRobot;
+
+public class rightSideAuton
+{
+    /** make it dance! */
+    public static IAutonStrategy simple(BillyRobot.AutonomousMecanumRobot robot)
+    {
+        return () ->
+        {
+            //start at a position on the white tape at the back of the field where it can shoot three preloaded balls
+            robot.mechAssembly.autonFlywheel.StartShoot();
+            ThreadExtensions.TrySleep(3000);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonFlywheel.StopShoot();
+
+            //drive to first row
+            robot.driveTrain.drive(0,0.5,0);
+            ThreadExtensions.TrySleep(500);
+            robot.driveTrain.turnToAngle(70);
+
+            //spinny intake collects 3 balls there
+            robot.mechAssembly.autonIntake.startIntake();
+            ThreadExtensions.TrySleep(3000);
+            robot.driveTrain.drive(0,0.5,0);
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonIntake.stopIntake();
+
+            //return to reg position
+            robot.driveTrain.drive(-0.5,-0.5,0);
+            ThreadExtensions.TrySleep(500);
+            robot.driveTrain.turnToAngle(-70);
+
+            //shoot balls
+            robot.mechAssembly.autonFlywheel.StartShoot();
+            ThreadExtensions.TrySleep(3000);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonFlywheel.StopShoot();
+
+            //go to second row of balls
+            robot.driveTrain.drive(0,0.5,0);
+            ThreadExtensions.TrySleep(1000);
+            robot.driveTrain.turnToAngle(70);
+
+            //spinny intake collects 3 balls there
+            robot.mechAssembly.autonIntake.startIntake();
+            ThreadExtensions.TrySleep(3000);
+            robot.driveTrain.drive(0,0.5,0);
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonIntake.stopIntake();
+
+            //go back to og pos
+            robot.driveTrain.drive(-0.5,-0.5,0);
+            ThreadExtensions.TrySleep(1000);
+            robot.driveTrain.turnToAngle(-70);
+
+            //shoot
+            robot.mechAssembly.autonFlywheel.StartShoot();
+            ThreadExtensions.TrySleep(3000);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonBallPusher.pushBalls();
+            ThreadExtensions.TrySleep(100);
+            robot.mechAssembly.autonFlywheel.StopShoot();
+
+            //go to ending position
+            robot.driveTrain.drive(0,0.5,0);
+            ThreadExtensions.TrySleep(1500);
+        };
+    }
+}
